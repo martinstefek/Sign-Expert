65d05ebf872d14934284d18598bf01fd790e8fd0
Martin Stefek <martin.stefek96@gmail.com>
2021-06-24 16:09:15 +0200

    Update project

--- public/index-logged-in.php
+++ public/index-logged-in.php
@@ -212,31 +212,5 @@
 
 <?php newsletter(); ?>
 
-<!--<div class="container">-->
-<!--    This is Index!-->
-<!---->
-<!--    <br>-->
-<!--    <br>-->
-<!--    <br>-->
-<!---->
-<!---->
-<!--    <div class="product-list-item-row">-->
-<!--        --><?php //for ($x = 1; $x <= (3); $x++): ?>
-<!--        <div class="product-list-item-col">-->
-<!--            --><?php //productListItem(12.75, 15.84, 124, true, true, 'Oracal 651G TURQUIOSE 054', 'Plotrové fólie'); ?>
-<!--        </div>-->
-<!---->
-<!--        <div class="product-list-item-col">-->
-<!--            --><?php //productListItem(null, null, 124, true, true, 'Oracal 651G TURQUIOSE 054', 'Plotrové fólie'); ?>
-<!--        </div>-->
-<!--        --><?php //endfor; ?>
-<!--    </div>-->
-<!---->
-<!--    <br>-->
-<!--    <br>-->
-<!--    <br>-->
-<!--    <br>-->
-<!--</div>-->
-
 
 <?php include_once('../php/layout/footer.php'); ?>
