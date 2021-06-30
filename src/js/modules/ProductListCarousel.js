833e43a1aea670c65993d795798c43a8fc5d092b
Martin Stefek <martin.stefek96@gmail.com>
2021-06-30 14:01:13 +0200

    Remarks

--- src/js/modules/ProductListCarousel.js
+++ src/js/modules/ProductListCarousel.js
@@ -7,23 +7,45 @@ $(document).ready(function() {
 
     // Definitions
     const slick = $('[data-type="product-list-carousel"]');
+    let isSlickActive = false;
 
-    slick.slick({
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        draggable: false,
-        arrows: true,
-        dots: true,
-        prevArrow: '<button class="slick-prev" aria-label="Predchádzajúci"><svg class="icon"><use xlink:href="#sprite-chevron-left"></use></svg></button>',
-        nextArrow: '<button class="slick-next" aria-label="Nasledujúci"><svg class="icon"><use xlink:href="#sprite-chevron-right"></use></svg></button>',
-        responsive: [
-            {
-                breakpoint: mobileNavigationThreshold,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+    const createSlick = () => {
+        if (isSlickActive) {
+            return;
+        }
+
+        slick.slick({
+            slidesToShow: 5,
+            slidesToScroll: 5,
+            draggable: false,
+            arrows: true,
+            dots: true,
+            prevArrow: '<button class="slick-prev" aria-label="Predchádzajúci"><svg class="icon"><use xlink:href="#sprite-chevron-left"></use></svg></button>',
+            nextArrow: '<button class="slick-next" aria-label="Nasledujúci"><svg class="icon"><use xlink:href="#sprite-chevron-right"></use></svg></button>',
+            responsive: [
+                {
+                    breakpoint: mobileNavigationThreshold,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
                 }
+            ]
+        });
+
+        isSlickActive = true;
+    };
+
+    createSlick();
+
+    window.addEventListener('resize', (e) => {
+        setTimeout(() => {
+            if (window.innerWidth <= 767) {
+                slick.slick('unslick');
+                isSlickActive = false;
+            } else {
+                createSlick();
             }
-        ]
+        }, 500);
     });
 });
